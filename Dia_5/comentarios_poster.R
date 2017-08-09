5e33e9bc6980e4ee9780930320452a75d28b3fa8
aeron15 <rescalante@immuneering.com>
2017-08-09 07:24:33 -0400

    Updated data and maps

--- Dia_5/comentarios_poster.R
+++ Dia_5/comentarios_poster.R
@@ -13,6 +13,6 @@ datos_df$localidad <- as.factor(datos_df$localidad)
 
 datos_df$localidad <- factor(datos_df$localidad, levels = c('huatulco_pa','acapulco','ixtapa','caleta_de_campos','revillagigedo','manzanillo','isabel','islas_marias','los_cabos','cabo_pulmo','isla_cerralvo','espiritu_santo','el_portugues','bajo_seco_sur','el_cayo','las_animas','isla_san_diego','isla_santa_cruz','isla_san_mateo','isla_el_farallon','bajo_seco_norte','loreto','punta_pulpito','isla_san_idelfonso','isla_san_marcos','isla_tortuga','isla_san_pedro_nolasco','isla_san_pedro_martir','san_francisquito','isla_san_lorenzo','isla_san_esteban','isla_tiburon','isla_patos','isla_angel_de_la_guarda','bahia_san_luis_gonzaga','puerto_libertad','puerto_lobos'))
 
-datos_df %>% ggplot(aes(x=localidad,y=densidad)) + geom_bar(stat='identity') + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+datos_df %>% ggplot(aes(x=localidad,y=densidad, fill = especie)) + geom_bar(stat='identity') + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + scale_fill_brewer(palette = "Set1")
 
 
